4afefe72389f5cbd269803cea24c0445ba9a72ae
unknown <lucianagmbw@gmail.com>
2021-01-08 23:43:10 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+
 import './App.css';
-import Hello from './Hello';
+import Home from './Home';
 import Navbar from './Navibar';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home  />
+    </>
   );
 }
 
